017fcc688bb18ca643a88dd84ff8a827a1497448
Sarah Lohmeier <sarahlohmes@gmail.com>
2018-04-02 18:25:04 -0400

    serves up an html home page
    Inspired by MDN's Node.js Server without a framework article: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Node_server_without_framework

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A bare-bones http server created with Node.js.
 
 Run `npm start` and go to http://localhost:3000.
+
+Original code from Node getting started guide: https://nodejs.org/en/docs/guides/getting-started-guide/
--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+  console.log('request ', req.url);
 
-server.listen(port, hostname, () => {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './index.html';
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      console.log('error ', err);
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(content, 'utf-8');
+    }
+  });
+
+}).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
